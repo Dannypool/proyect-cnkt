9d02a99192c0ed91f78caa75f6ca9857de41e67e
Dannypool <dan.castro023@gmail.com>
2018-11-26 02:58:20 -0600

    ADD > advance in user view

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(page: number): Observable<any>{
+    return this.http.get<any>('https://reqres.in/api/users?page=' + page);
+  }
+
+  getUserById(id: number): Observable<any>{
+    return this.http.get<any>('https://reqres.in/api/users/' + id);
+  }
+
+  deleteUserById(id: number): Observable<any>{
+    return this.http.delete<any>('https://reqres.in/api/users/' + id);
+  }
+
+  createUser(name: string, job: string): Observable<any>{
+    return this.http.post<any>('https://reqres.in/api/users', {
+      name: name,
+      job: job
+    });
+  }
+
+}
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SpinnerService } from '../services/spinner.service';
+import { UsersService } from '../services/users.service';
+import { Users } from '../models/users';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +10,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  public page: number = 1
+  public pages: Array<number> = new Array(1)
+  public users: Users[]
+
+  constructor(
+    private usersService: UsersService,
+    private spinnerService: SpinnerService
+  ) { }
 
   ngOnInit() {
+    this.getUsers()
+  }
+
+  getUsers(){
+    this.spinnerService.showSpinner()
+    this.usersService.getUsers(this.page)
+      .subscribe(
+        r => {
+          this.users = r['data']
+          this.pages = new Array(r['total_pages'])
+        },
+        r => {},
+        () => {
+          this.spinnerService.hideSpinner()
+        }
+      )
+  }
+
+  getUser(id: number){
+    this.spinnerService.showSpinner()
+    this.usersService.getUserById(id)
+      .subscribe(
+        r => {
+          
+        },
+        r => {},
+        () => {
+          this.spinnerService.hideSpinner()
+        }
+      )
+  }
+
+  deleteUser(id: number){
+    this.spinnerService.showSpinner()
+    this.usersService.deleteUserById(id)
+      .subscribe(
+        r => {
+          
+        },
+        r => {},
+        () => {
+          this.spinnerService.hideSpinner()
+        }
+      )
+  }
+
+  changePage(i: number){
+    this.page = i + 1
+    this.getUsers()
+  }
+
+  previousPage(){
+    if (this.page > 1) {
+      this.page--
+      this.getUsers()
+    }
+  }
+
+  nextPage(){
+    if (this.page < this.pages.length) {
+      this.page++
+      this.getUsers()
+    }
   }
 
 }
